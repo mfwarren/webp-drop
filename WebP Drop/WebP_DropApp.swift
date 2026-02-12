8e4b696ac2ea606080722b65f4ddd3a2112e0962
Matt Warren <matt.warren@gmail.com>
2026-02-12 16:13:20 -0500

    Initial Commit

---
+++ WebP Drop/WebP_DropApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebP_DropApp.swift
+//  WebP Drop
+//
+//  Created by Matt Warren on 2026-02-12.
+//
+
+import SwiftUI
+
+@main
+struct WebP_DropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
